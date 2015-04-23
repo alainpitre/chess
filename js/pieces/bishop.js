74a1e852e9d110789c69759883fd3cd0a0e32296
Alain Pitre <alain.pitre@multilearning.com>
2015-04-23 16:55:02 -0400

    test bishop first diagonal

--- js/pieces/bishop.js
+++ js/pieces/bishop.js
@@ -3,8 +3,27 @@ function Bishop(player){
 	var public = new Piece("bishop", player);
 	var private = {};
 
+	var x = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+	var y = ['1', '2', '3', '4', '5', '6', '7', '8'];
+
 	private.construct = function(){
 		public.setHtml("Bi");
+	};
+
+	public.getMoves = function(){
+		var currentPosition = public.getPosition();
+		return private.getDiagonal(currentPosition);
+	}
+
+	private.getDiagonal = function(position){
+		var move = [];
+		for(var i = 1; i < 6; i++){
+			move.push({
+				'col' : x[5 - i],
+				'row' : y[7 - i]
+			});
+		}
+		return move;
 	};
 
 	private.construct();
